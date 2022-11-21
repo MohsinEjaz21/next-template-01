25209069a7ebe0c4745f68ec39eb3eab879f8a02
Mohsin Ejaz <mejaz.bese21@gmail.com>
2022-11-22 01:00:28 +0500

    Adding styles in assets

--- layout/index.tsx
+++ layout/index.tsx
@@ -1,9 +1,12 @@
+import Script from 'next/script';
 import Header from '../components/Header';
 
 function Layout({ children }) {
   return (
     <>
       <Header />
+      <Script src="assets/scripts/typetura.min.js" />
+
       <main data-barba="container">
         {children}
       </main>
