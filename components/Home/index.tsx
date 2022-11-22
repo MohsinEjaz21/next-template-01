d3493f5c914fc85eb243a59dc2728e865fdbd830
Mohsin Ejaz <mejaz.bese21@gmail.com>
2022-11-22 12:48:53 +0500

    Still adding css reset

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -1,12 +1,9 @@
-import React from 'react'
-import Section_01 from './Section_01'
-import Section_02 from './Section_02'
 
 function Home() {
   return (
     <div className="home">
-      <Section_01/>
-      <Section_02/>
+      {/* <Section_01/>
+      <Section_02/> */}
     </div>
   )
 }
