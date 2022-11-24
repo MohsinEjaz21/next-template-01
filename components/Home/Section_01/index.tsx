5d05b2ea6cb3ad063a095838f80a6b4bbbac0340
Mohsin Ejaz <mejaz.bese21@gmail.com>
2022-11-24 20:00:19 +0500

    Working on image

--- components/Home/Section_01/index.tsx
+++ components/Home/Section_01/index.tsx
@@ -3,10 +3,16 @@ import BtnPrimary from "@/components/widgets/Buttons/BtnPrimary"
 function Section_01() {
   return (
     <section className="section_01">
-      <h4>WHO WE ARE</h4>
-      <h2>CENTRE FOR AEROSPACE & SECURITY STUDIES</h2>
-      <p>The “Centre for Aerospace & Security Studies” (CASS) is an independent think tank located in Islamabad, Pakistan. CASS is unique in its specialization of Aerospace, as the only think tank in Pakistan to carry out systematic research in the domain. The aerospace sector is a key driver behind globalization, playing a pivotal role in national security, economic development and in supporting long-term economic growth to facilitate a country’s integration into the global economy. </p>
-      <BtnPrimary label='More About Us' />
+      <div className="frame_01">
+        <h4>WHO WE ARE</h4>
+        <h2>CENTRE FOR AEROSPACE & SECURITY STUDIES</h2>
+        <p>The “Centre for Aerospace & Security Studies” (CASS) is an independent think tank located in Islamabad, Pakistan. CASS is unique in its specialization of Aerospace, as the only think tank in Pakistan to carry out systematic research in the domain. The aerospace sector is a key driver behind globalization, playing a pivotal role in national security, economic development and in supporting long-term economic growth to facilitate a country’s integration into the global economy. </p>
+        <BtnPrimary label='More About Us' />
+      </div>
+      <div className="frame_02">
+        <img src={"https://picsum.photos/seed/picsum/700/700"} alt="" />
+
+      </div>
     </section>
   )
 }
