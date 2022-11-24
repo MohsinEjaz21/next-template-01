f485732a33e76292cd8ba123852135114932be9a
Mohsin Ejaz <mejaz.bese21@gmail.com>
2022-11-24 23:36:22 +0500

    cleanup

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -1,11 +1,10 @@
 import Section_01 from "./Section_01"
-import Section_02 from "./Section_02"
 
 function Home() {
   return (
     <div className="home">
       <Section_01 />
-      <Section_02 />
+      {/* <Section_02 /> */}
     </div>
   )
 }
