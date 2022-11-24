c2f73d2e66ee001ba4ff04af5f6a151e965bcd78
Mohsin Ejaz <mejaz.bese21@gmail.com>
2022-11-24 22:52:18 +0500

    cleanup

--- components/Home/Section_01/index.tsx
+++ components/Home/Section_01/index.tsx
@@ -4,7 +4,7 @@ function Section_01() {
   return (
     <section className="section_01">
       <div className="frame_01">
-        <h4>WHO WE ARE</h4>
+        <h3>WHO WE ARE</h3>
         <h2>CENTRE FOR AEROSPACE & SECURITY STUDIES</h2>
         <p>The “Centre for Aerospace & Security Studies” (CASS) is an independent think tank located in Islamabad, Pakistan. CASS is unique in its specialization of Aerospace, as the only think tank in Pakistan to carry out systematic research in the domain. The aerospace sector is a key driver behind globalization, playing a pivotal role in national security, economic development and in supporting long-term economic growth to facilitate a country’s integration into the global economy. </p>
         <BtnPrimary label='More About Us' />
