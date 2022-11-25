151ca7c0a86ca70b7eaea45f28ffcd9ebd1ae0ea
Mohsin Ejaz <mejaz.bese21@gmail.com>
2022-11-25 10:44:50 +0500

    cleanup

--- components/Home/HomeDataSource.tsx
+++ components/Home/HomeDataSource.tsx
@@ -9,17 +9,11 @@ export const HomeDataSource = {
   section02: {
     text01: 'Message From the President',
     text02: 'Air Marshal Farhat Hussain Khan (Retd)',
-    text03: <>
-      CASS was established in December 2018 to fill the knowledge and research gaps that existed in domains of Aerospace and National Security in an otherwise think tank rich capital, Islamabad.
-      <br /><br />
-      To achieve our objective of being amongst the best think tanks of the region and with a focused mission, CASS has made significant contribution in providing policy guidelines on important issues of national interest, especially security, both in military and non-military domains disseminated through direct as well indirect engagement on a regular basis.
-      <br /><br />
-      As part of the Advisory Board of the Government of Pakistan’s National Security Division, CASS has and would continue to serve as a thought leader, by providing independent, innovative, and evidence-based solutions to strengthen Pakistan’s national security including specific emerging geostrategic and geo-economic situations.
-      <br /><br />
-      Maintaining a winning Aerospace Power requires high-end technologies that are not only cost-intensive, but their availability with the passage of time is likely to be challenging. In our region, these technologies are largely purchased or borrowed. Therefore, working towards our vision of strengthening National Security in all its forms, through indigenization, CASS would continue to lay emphasis on innovative research on emerging technologies, including but not limited to cyber and space. We continue to enhance our outreach through greater use of media, seminars, webinars, digital technologies, joint research and cementing our relationships with domestic, regional, and global partners.
-      <br /><br />
-      InshAllah, in the times ahead, we will continue to build upon our achievements by setting even higher standards of research for inputs in the national policymaking process.
-    </>,
+    text03: <>`CASS was established in December 2018 to fill the knowledge and research gaps that existed in domains of Aerospace and National Security in an otherwise think tank rich capital, Islamabad.
+      <br /><br /> To achieve our objective of being amongst the best think tanks of the region and with a focused mission, CASS has made significant contribution in providing policy guidelines on important issues of national interest, especially security, both in military and non-military domains disseminated through direct as well indirect engagement on a regular basis.
+      <br /><br /> As part of the Advisory Board of the Government of Pakistan’s National Security Division, CASS has and would continue to serve as a thought leader, by providing independent, innovative, and evidence-based solutions to strengthen Pakistan’s national security including specific emerging geostrategic and geo-economic situations.
+      <br /><br /> Maintaining a winning Aerospace Power requires high-end technologies that are not only cost-intensive, but their availability with the passage of time is likely to be challenging. In our region, these technologies are largely purchased or borrowed. Therefore, working towards our vision of strengthening National Security in all its forms, through indigenization, CASS would continue to lay emphasis on innovative research on emerging technologies, including but not limited to cyber and space. We continue to enhance our outreach through greater use of media, seminars, webinars, digital technologies, joint research and cementing our relationships with domestic, regional, and global partners.
+      <br /><br /> InshAllah, in the times ahead, we will continue to build upon our achievements by setting even higher standards of research for inputs in the national policymaking process.`</>,
     image: 'https://casstt.com/wp-content/uploads/2022/11/President.jpg'
   }
 }
--- components/Home/Section_02/index.tsx
+++ components/Home/Section_02/index.tsx
@@ -1,11 +1,24 @@
+import { useEffect, useState } from "react";
+import ReactDOMServer from 'react-dom/server';
 import { HomeDataSource } from "../HomeDataSource";
 
 const data = HomeDataSource.section02;
 
 function Section_02() {
 
-  const showMoreTextLabel = (text) => {
+  const [text, setText] = useState();
 
+  useEffect(() => {
+    const plainText = ReactDOMServer.renderToString(data.text03)
+    console.log(plainText)
+  }, [])
+
+  const elipseLargeText = (text, size) => {
+    let elipseText = text;
+    if (text.length > size) {
+      elipseText = text.substring(0, size + 1) + "...";
+    }
+    return <>{elipseText}</>
   }
 
   return (
@@ -16,7 +29,7 @@ function Section_02() {
       <div className="frame_02">
         <h3>{data.text01}</h3>
         <h2>{data.text02}</h2>
-        <p>{data.text03} </p>
+        <p>{data.text03}</p>
       </div>
     </section>
   )
