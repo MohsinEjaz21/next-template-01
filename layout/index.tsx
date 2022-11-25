a2f1f23eb2932d96c2d249e366c5f7672e186374
Mohsin Ejaz <mejaz.bese21@gmail.com>
2022-11-25 17:57:28 +0500

    working on dark mode

--- layout/index.tsx
+++ layout/index.tsx
@@ -3,14 +3,14 @@ import Header from '../components/Header';
 
 function Layout({ children }) {
   return (
-    <>
+    <div >
       <Header />
       <Script src="assets/scripts/typetura.min.js" />
 
-      <main data-barba="container">
+      <main data-barba="container" custom-theme="dark">
         {children}
       </main>
-    </>
+    </div>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <main className="dark">
 
       </main>
 
