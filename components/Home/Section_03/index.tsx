881a0d5d28a360bd0db74dbd91dd818842c74bff
Mohsin Ejaz <mejaz.bese21@gmail.com>
2022-11-26 00:10:11 +0500

    cleanup

---
+++ components/Home/Section_03/index.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import ReactDOMServer from 'react-dom/server';
+import ElipseText from "../../widgets/ElipseText";
+import { HomeDataSource } from "../HomeDataSource";
+
+const data = HomeDataSource.section02;
+
+function Section_03() {
+
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    const plainText = ReactDOMServer.renderToString(data.text03)
+    setText(plainText);
+    console.log(plainText)
+  }, [])
+
+  return (
+    <section className="section_03" data-scroll-section>
+      <div className="frame_01" >
+        <img src={data.image} alt="" />
+      </div>
+      <div className="frame_02" >
+        <h4>{data.text01}</h4>
+        <h2>{data.text02}</h2>
+        <ElipseText text={text} size={554} />
+      </div>
+    </section>
+  )
+}
+
+export default Section_03
--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -1,11 +1,14 @@
 import Section_01 from "./Section_01"
 import Section_02 from "./Section_02"
+import Section_03 from "./Section_03"
 
 function Home() {
   return (
     <div className="home">
       <Section_01 />
       <Section_02 />
+      <Section_03 />
+
     </div>
   )
 }
