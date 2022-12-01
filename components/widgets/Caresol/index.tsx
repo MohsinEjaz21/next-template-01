ca6e4a3ba0b92ae52b2a92c3750471b12952d464
Mohsin Ejaz <mejaz.bese21@gmail.com>
2022-12-01 13:08:23 +0500

    Update index.tsx

--- components/widgets/Caresol/index.tsx
+++ components/widgets/Caresol/index.tsx
@@ -1,34 +1,34 @@
-import { Navigation } from "swiper";
+import { Autoplay, Navigation } from "swiper";
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 const data = [
   {
-    imgUrl: 'https://images.squarespace-cdn.com/content/v1/613f323315e7c403d0d6c265/1634039725468-H44I8D4SQUZ6ONK32JY2/Frame+5.png?format=1000w',
+    imgUrl: 'https://casstt.com/wp-content/uploads/2022/11/Amb_Jilani_W2.jpg',
     text01: 'Electric Cars',
     text02: 'low weight, power'
   },
   {
-    imgUrl: 'https://images.squarespace-cdn.com/content/v1/613f323315e7c403d0d6c265/1634035057033-EUFDFWKLA02GJ2BAUN69/Frame+2.png?format=1000w',
+    imgUrl: 'https://casstt.com/wp-content/uploads/2022/11/AM-Ashfaque1.png',
     text01: 'Electric Bikes',
     text02: 'high performance'
   },
   {
-    imgUrl: 'https://images.squarespace-cdn.com/content/v1/613f323315e7c403d0d6c265/1634035102964-STPUCV657Q4ILAWM5ID2/Frame+1.png?format=1000w',
+    imgUrl: 'https://casstt.com/wp-content/uploads/2022/11/AM_Habib_2.jpg',
     text01: 'Drones',
     text02: 'low weight, power'
   },
   {
-    imgUrl: 'https://images.squarespace-cdn.com/content/v1/613f323315e7c403d0d6c265/1634035138932-DPVUF3IME3MFOXW79XC0/Frame+4.png?format=1000w',
+    imgUrl: 'https://casstt.com/wp-content/uploads/2022/11/AVM-Faheem1.png',
     text01: 'Industrial vehicles',
     text02: 'longevity, sustainability'
   },
   {
-    imgUrl: 'https://images.squarespace-cdn.com/content/v1/613f323315e7c403d0d6c265/1634039785060-CIKMULMME5DNQ3CJXJ25/Frame+6.png?format=1000w',
+    imgUrl: 'https://casstt.com/wp-content/uploads/2022/11/Dr._Shamsi_.jpg',
     text01: 'Residential',
     text02: 'longevity, sustainability'
   },
   {
-    imgUrl: 'https://images.squarespace-cdn.com/content/v1/613f323315e7c403d0d6c265/1634039785060-CIKMULMME5DNQ3CJXJ25/Frame+6.png?format=1000w',
+    imgUrl: 'https://casstt.com/wp-content/uploads/2022/11/Dr_Usman1.jpg',
     text01: 'Residential',
     text02: 'longevity, sustainability'
   },
@@ -42,7 +42,11 @@ const sliderProps = {
   cssMode: true,
   spaceBetween: 50,
   slidesPerView: 3,
-  modules: [Navigation],
+  modules: [Navigation, Autoplay],
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   // Responsive breakpoints
   breakpoints: {
     300: {
